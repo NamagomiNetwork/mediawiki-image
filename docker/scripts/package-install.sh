0e21a6100fff3ffd5bac1ac693426302ea1e9bd8
nabr <100725601+nabr2730@users.noreply.github.com>
2022-06-29 04:06:31 +0900

    TZを指定

--- docker/scripts/package-install.sh
+++ docker/scripts/package-install.sh
@@ -5,6 +5,8 @@ set -e
 sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://jp.archive.ubuntu.com/ubuntu/%g" /etc/apt/sources.list
 sed -i.bak -e "s%http://security.ubuntu.com/ubuntu/%http://jp.archive.ubuntu.com/ubuntu/%g" /etc/apt/sources.list
 
+TZ=Asia/Tokyo
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update && apt-get upgrade -y
 
 apt-get install -y \
